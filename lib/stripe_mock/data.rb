dccf7d5f6e99a95b4b4fd1884d9d709314c6431a
Luciano Bandeira <luciano062@gmail.com>
2025-11-14 10:54:18 -0300

    fix: add billing_details attribute to charge

--- lib/stripe_mock/data.rb
+++ lib/stripe_mock/data.rb
@@ -223,6 +223,19 @@ module StripeMock
         amount: 0,
         application_fee: nil,
         application_fee_amount: nil,
+        billing_details: {
+          address: {
+            city: 'New Orleans',
+            country: 'US',
+            line1: 'Bourbon Street 23',
+            line2: nil,
+            postal_code: '10000',
+            state: nil
+          },
+          email: 'foo@bar.com',
+          name: 'John Dolton',
+          phone: nil
+        },
         currency: currency,
         destination: nil,
         fraud_details: {},
